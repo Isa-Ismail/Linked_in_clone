46602b794b48aac69f9ebdc6fe52d876c1300651
Fahim_tech_support <fahimismail75.scc@gmail.com>
2022-01-17 02:36:28 +0600

    'o-auth'

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,7 +1,12 @@
-function HeaderLink({Icon, text}) {
+function HeaderLink({Icon, text, avatar, feed}) {
     return (
-        <div>
-            <h1>{text}</h1>
+        <div className ={ `cursor-pointer flex flex-col items-center
+         ${
+             feed? 'text-black/60 hover:text-black dark:text-white/75 dark:hover:text-white lg:-mb-1.5 space-y-1':
+                'text-grey-500 hover:text-cyan-700'}` 
+        }>
+            {avatar?(<Icon className = '!h-7 !w-7 lg:!-mb-1'/>):(<Icon />)}
+            <h4 className = 'text-sm'>{text}</h4>
         </div>
     )
 }
--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import Image from "next/image"
+import Link from "next/link";
 import HeaderLink from '../components/HeaderLink'
 import ExploreIcon from "@mui/icons-material/Explore";
 import GroupIcon from "@mui/icons-material/Group";
@@ -9,26 +10,64 @@ import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRound
 
 function HomePage() {
     return (
-        <div>
+        <div className = 'space-y-10 relative'>
             <Head>
                 <title>LinkedIn</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <header className = 'flex justify-around py-3.5 px-3.5'>
-                <div className = "relative w-36 h-10 ">
-                    <Image src="https://rb.gy/vtbzlp" layout="fill" objectFit="contain" />
+
+            <header className = 'flex justify-around py-3.5 px-10 bg-violet-50'>
+                <div className = "relative w-36 h-10 cursor-pointer">
+                    <Link href = '/'>
+                        <Image src="https://rb.gy/vtbzlp" layout="fill" objectFit="contain" />
+                    </Link>
                 </div>
                 <div className = 'flex-grow'></div>
                 <div className = 'flex items-center sm:divide-x divide-gray-300'>
                     <div className = 'hidden sm:flex space-x-8 px-4'>
-                    <HeaderLink Icon={ExploreIcon} text="Discover" />
-                    <HeaderLink Icon={GroupIcon} text="People" />
-                    <HeaderLink Icon={OndemandVideoSharpIcon} text="Learning" />
-                    <HeaderLink Icon={BusinessCenterIcon} text="Jobs" />
+                        <HeaderLink Icon={ExploreIcon} text="Discover" />
+                        <HeaderLink Icon={GroupIcon} text="People" />
+                        <HeaderLink Icon={OndemandVideoSharpIcon} text="Learning" />
+                        <HeaderLink Icon={BusinessCenterIcon} text="Jobs" />
+                    </div>
+                    <div className = 'pl-6'>
+                        <button 
+                         className = 'text-blue-700 font-semibold rounded-full border border-blue-800 px-5 py-1.5 bg-transparent hover:bg-blue-800 hover:text-white'
+                         onClick = {() => {}}>
+                            Sign in
+                        </button>
                     </div>
                 </div>
-
             </header>
+
+            <main className="pb-20 flex flex-col xl:flex-row items-center max-w-screen-lg mx-auto">
+                <div className="space-y-6 xl:space-y-10 mt-20">
+                    <h1 className="text-3xl md:text-5xl text-amber-800/80 max-w-xl !leading-snug pl-4 xl:pl-0">
+                        Welcome to your professional community
+                    </h1>
+                    <div className="space-y-4">
+                        <div className="intent">
+                            <h2 className="text-xl">Search for a job</h2>
+                            <ArrowForwardIosRoundedIcon className="text-gray-700" />
+                        </div>
+                        <div className="intent">
+                            <h2 className="text-xl">Find a person you know</h2>
+                            <ArrowForwardIosRoundedIcon className="text-gray-700" />
+                        </div>
+                        <div className="intent">
+                            <h2 className="text-xl">Learn a new skill</h2>
+                            <ArrowForwardIosRoundedIcon className="text-gray-700" />
+                        </div>
+                    </div>
+                    <div className = 'relative xl:absolute w-80 h-80 xl:w-[650px] xl:h-[650px] top-14 right-5'>
+                        <Image src = 'https://rb.gy/vkzpzt' layout="fill" objectFit="contain" />
+                    </div>
+                </div>
+            </main>
+            <footer className = 'bg-emerald-700'>
+
+            </footer>
+
         </div>
     )
 }

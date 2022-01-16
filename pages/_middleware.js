c810929abaa73b3f5b6e11ba22bc620d04b5baba
Fahim_tech_support <fahimismail75.scc@gmail.com>
2022-01-17 04:04:42 +0600

    'saved-all'

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,7 +2,6 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-    
   if (req.nextUrl.pathname === "/") {
     const session = await getToken({
       req,
